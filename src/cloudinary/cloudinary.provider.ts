d25396003fe00363df1ec75d15756494374b4cf8
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2023-08-04 15:53:01 +0700

    export cloudinary config to env

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -5,9 +5,9 @@ export const CloudinaryProvider = {
   provide: CLOUDINARY,
   useFactory: () => {
     return v2.config({
-      cloud_name: 'drrw0n4eg',
-      api_key: '794242867665624',
-      api_secret: '8nW_RQouSPaS-NYmm6xMT6BJuaI',
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
       secure: true,
     });
   },
